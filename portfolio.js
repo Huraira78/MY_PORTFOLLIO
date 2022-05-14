1533c90339fa56aa7bfc5a8eb54b51c306794c37
Huraira78 <2019cs661@student.uet.edu.pk>
2022-05-14 18:05:48 +0500

    First commit

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+var skills={
+    strings:['Web Developer','Designer'],
+    typeSpeed:100,
+    backSpeed:60,
+    backDelay:900,
+    startDelay:1500,
+    showCursor:false,
+    loop:true
+};
+var typing=new Typed('.skill',skills)
